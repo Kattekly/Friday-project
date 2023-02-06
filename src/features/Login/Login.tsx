4312efb665c88141dedd9bf99f607c79813b0182
Ekaterina <kattekly@mail.ru>
2023-02-06 14:48:21 +0300

    login

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,72 +1,86 @@
+
+import { useFormik } from 'formik'
+import { Navigate, NavLink } from 'react-router-dom'
+import SuperInputText from '../../common/SuperInputText/SuperInputText'
+
+import SuperButton from '../../common/SuperButton/SuperButton'
 import React from 'react'
-import {useFormik} from 'formik'
+import s from './Login.module.css'
 import {useDispatch, useSelector} from 'react-redux'
+import SuperCheckbox from '../../common/SuperCheckbox/SuperCheckbox'
 import {AppRootStateType} from "../../App/stor";
-import {Button, Checkbox, FormControlLabel, TextField} from "@mui/material";
-import SuperInputText from "../../common/SuperInputText/SuperInputText";
-import s from './Login.module.scss'
 
-
-export const Login = () => {
+type FormikErrorType = {
+    email?: string
+    password?: string
+    rememberMe?: boolean
+}
+ export const Login = () => {
     const dispatch = useDispatch()
-    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.login.isLoggedIn);
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.login.isLoggedIn)
 
     const formik = useFormik({
-        validate: (values) => {
-            if (!values.email) {
-                return {
-                    email: 'Email is required'
-                }
-            }
-            if (!values.password) {
-                return {
-                    password: 'Password is required'
-                }
-            }
-
-        },
         initialValues: {
             email: '',
             password: '',
-            rememberMe: false
+            rememberMe: false,
+        },
+        validate: values => {
+            const errors: FormikErrorType = {}
+
+            if (!values.email) {
+                errors.email = 'Required'
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address'
+            }
+            if (values.password.length < 9) {
+                errors.password = 'Password should be more 8 letters'
+            }
+
+            return errors
         },
         onSubmit: values => {
-            /*dispatch(myLoginTC(values));*/
+            // dispatch(loginTC(values))
+            formik.resetForm() //зачистить поле
         },
     })
+    if (isLoggedIn) {
+        return <Navigate to={'/profile'} />
+    }
 
-    /*if (isLoggedIn) {
-        return <Redirect to={"/"} />
-    }*/
+    return (
+        <div >
+            <h3>Sign in</h3>
+            <form onSubmit={formik.handleSubmit}>
 
+                <label className={formik.touched.email && formik.errors.email ? s.errorField : ''}>
+                    Email
+                    <SuperInputText type={'text'} {...formik.getFieldProps('email')} />
+                    <div className={s.error}>{formik.touched.email && formik.errors.email && formik.errors.email}</div>
+                </label>
 
-    return <div>
-        <h3>Sign in</h3>
-        <form onSubmit={formik.handleSubmit}>
+                <label className={formik.touched.password && formik.errors.password ? s.errorField : ''}>
+                    Password
+                    {/*<input type="password" name="password" />*/}
 
-            <label className={formik.touched.email && formik.errors.email ? s.errorField : ''}>
-                Email
-                <SuperInputText type={'text'} {...formik.getFieldProps('email')} />
-                <div className={s.error}>{formik.touched.email && formik.errors.email && formik.errors.email}</div>
-            </label>
+                    <div className={s.error}>{formik.touched.password && formik.errors.password && formik.errors.password}</div>
+                </label>
 
+                <div className={s.checkboxField}>
+                    <SuperCheckbox {...formik.getFieldProps('rememberMe')}>Remember me</SuperCheckbox>
+                </div>
 
-            <TextField
-                type="password"
-                label="Password"
-                margin="normal"
-                {...formik.getFieldProps("password")}
-            />
-            {formik.errors.password ? <div>{formik.errors.password}</div> : null}
-            <FormControlLabel
-                label={'Remember me'}
-                control={<Checkbox
-                    {...formik.getFieldProps("rememberMe")}
-                    checked={formik.values.rememberMe}
-                />}
-            />
-            <Button type={'submit'} variant={'contained'} color={'primary'}>Login</Button>
-
-        </form>
-    </div>
+                <p>
+                    <NavLink to="/recoveryPassword">Forgot Password?</NavLink>
+                </p>
+                <div className={s.sendBtn}>
+                    <SuperButton xType={'default'} type="submit">
+                        Sign in
+                    </SuperButton>
+                </div>
+            </form>
+            <p>Already have an account?</p>
+            <NavLink to="/signUp">Sign Up</NavLink>
+        </div>
+    )
 }
